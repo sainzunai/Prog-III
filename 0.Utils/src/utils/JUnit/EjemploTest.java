d3276ea9196ae010d6f86a54c1c07fedaeb5f799
UD <UD@DESKTOP-U94OR0T>
2018-10-29 21:05:34 +0100

    a

--- 0.Utils/src/utils/JUnit/EjemploTest.java
+++ 0.Utils/src/utils/JUnit/EjemploTest.java
@@ -1,5 +1,26 @@
 package utils.JUnit;
 
-public class EjemploTest {
+import static org.junit.Assert.fail;
+
+import org.junit.*;
+
+
+class EjemploTest {
+
+	@Before
+	public void setUp(){
+		fail("");
+	}
+
+	@After
+	public void tearDown() {
+		fail("");
+	}
+
+
+	@Test(expected = IndexOutOfBoundsException.class)
+	public void testGet_Exc1() {
+		fail("");
+	}
 
 }
