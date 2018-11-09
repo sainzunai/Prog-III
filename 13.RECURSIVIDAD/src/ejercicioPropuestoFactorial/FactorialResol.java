e4d36b0ed30e9517fc3fd56f61727186adea6a36
sainzunai <33113726+sainzunai@users.noreply.github.com>
2018-11-09 08:29:02 +0100

    otra opcion de hacer el factorial

--- 13.RECURSIVIDAD/src/ejercicioPropuestoFactorial/FactorialResol.java
+++ 13.RECURSIVIDAD/src/ejercicioPropuestoFactorial/FactorialResol.java
@@ -4,7 +4,16 @@ public class FactorialResol {
 
 	public static void main(String[] args) {
 		factorial(14,1);
+		factorial2( 5 );
 
+	}
+
+	private static int factorial2(int i) {
+		if (i==0) {
+			return 1;
+		}else {
+			return i*factorial2(i-1);
+		}
 	}
 
 	private static void factorial(int objetivo, int valor) {
