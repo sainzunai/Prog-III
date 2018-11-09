c01e628524d5fa17694531377d906279f6f3c84e
sainzunai <33113726+sainzunai@users.noreply.github.com>
2018-11-09 18:09:22 +0100

    resoluciones de clase implementadas

--- 13.RECURSIVIDAD/src/ejercicioPropuestoFactorial/FactorialResol.java
+++ 13.RECURSIVIDAD/src/ejercicioPropuestoFactorial/FactorialResol.java
@@ -3,25 +3,26 @@ package ejercicioPropuestoFactorial;
 public class FactorialResol {
 
 	public static void main(String[] args) {
-		factorial(14,1);
-		factorial2( 5 );
+		factorial(14, 1); // podemos hacer un metodo a parte para que solo tengamos que mandar un
+							// parametro y que no se vean los patametros funcionales
+		factorial2(5);
 
 	}
 
 	private static int factorial2(int i) {
-		if (i==0) {
+		if (i == 0) {
 			return 1;
-		}else {
-			return i*factorial2(i-1);
+		} else {
+			return i * factorial2(i - 1);
 		}
 	}
 
 	private static void factorial(int objetivo, int valor) {
 		if (objetivo == 0) {
-			System.out.println( valor );
-		}else {
+			System.out.println(valor);
+		} else {
 			int siguiente = objetivo * valor;
-			factorial( objetivo - 1, siguiente);
+			factorial(objetivo - 1, siguiente);
 		}
 	}
 
