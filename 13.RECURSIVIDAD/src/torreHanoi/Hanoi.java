dafe2f895773afda8e753c46b9a774a551e5cb1a
sainzunai <33113726+sainzunai@users.noreply.github.com>
2018-11-16 08:37:36 +0100

    ejemplo terminado

--- 13.RECURSIVIDAD/src/torreHanoi/Hanoi.java
+++ 13.RECURSIVIDAD/src/torreHanoi/Hanoi.java
@@ -3,9 +3,17 @@ package torreHanoi;
 public class Hanoi {
 
 	public static void main(String[] args) {
-		hanoi();
+		hanoi(3, 'a', 'b', 'c');
 	}
-private static void hanoi() {
-	
-}
+
+	private static void hanoi(int n, char origen, char destino, char auxiliar) {
+		if (n == 1) {
+			System.out.println("Mover 1 de " + origen + " a " + destino);
+		}else {
+			hanoi(n-1, origen, auxiliar, destino);
+			System.out.println("Mover " + n + " de " + origen + " a " + destino);
+			hanoi(n-1, auxiliar, destino, origen);
+		}
+		
+	}
 }
